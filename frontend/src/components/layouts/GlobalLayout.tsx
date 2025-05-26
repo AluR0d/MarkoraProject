1d602c8bdb9c9529f6b1a23b8667216f861ddc8b
AluR0d <adrian.luque.rod@gmail.com>
2025-05-26 14:56:43 +0200

    css-fix

--- frontend/src/components/layouts/GlobalLayout.tsx
+++ frontend/src/components/layouts/GlobalLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import MainNavbar from '../organisms/MainNavbar';
 import Container from '@mui/material/Container';
 import FancyNavbar from '../organisms/FancyNavbar';
 
 export default function GlobalLayout() {
   return (
     <>
-      {/* <MainNavbar /> */}
       <FancyNavbar />
       <Container maxWidth="lg" sx={{ mt: 4 }}>
         <Outlet />
